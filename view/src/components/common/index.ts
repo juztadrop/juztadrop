da3afc1abb54565433efc91c093d0761f3b2ef11
Floze <88098863+floze-the-genius@users.noreply.github.com>
2026-07-19 05:32:40 +0400

    feat(view): add reusable error message component

--- view/src/components/common/index.ts
+++ view/src/components/common/index.ts
@@ -1,2 +1,4 @@
 export { InProgressPage } from './InProgressPage';
 export { Dropdown } from './DropDown';
+export { ErrorMessage } from './ErrorMessage';
+export type { ErrorMessageProps, ErrorMessageVariant } from './ErrorMessage';
